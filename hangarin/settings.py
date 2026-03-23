d14f399608fce1044a005978315a21d05e1501f6
Fernandez <202180420@psu.palawan.edu.ph>
2026-03-23 22:39:14 +0800

    Add STATIC_ROOT and update ALLOWED_HOSTS

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--_zz)rr5!)zygaf6slojunzsveqmp7mvkqr=_x^e5m0o8%hp)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
